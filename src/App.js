6b61c866520ac7fe0c203b6f6d48c63c9713df36
01Bavi <gbavitha09@gmail.com>
2024-09-27 12:21:20 +0530

    profile dropdown aligned

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import Formcomponent from './components/Formcomponent';
 import DisplayForm from './components/DisplayForm';
 import Selectcomponent from './components/Selectcomponent'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Userloginitem from './components/Userloginitem';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Userloginitem />} />
+        
         <Route path="/" element={<Selectcomponent />} />
 
         <Route path="/" element={<Formcomponent />} />
---
+++ src/components/Userloginitem.js
@@ -0,0 +1,82 @@
+import React , {useState}from 'react';
+import { LuUserCircle2 } from "react-icons/lu";
+import { MdFeedback } from "react-icons/md";
+import { RiSettings5Fill } from "react-icons/ri";
+import { IoHelpCircleSharp } from "react-icons/io5";
+import { MdDarkMode } from "react-icons/md";
+import { IoLogOutSharp } from "react-icons/io5";
+import { MdNavigateNext } from "react-icons/md";
+import { IoMdArrowDropdownCircle } from "react-icons/io";
+import '../styles/Userloginitem.css'; 
+
+const Userloginitem = () => {
+    const [open, setOpen] = useState(false);
+
+    const openDropdown = ()=>{
+        setOpen(!open);
+    }
+
+    return (
+        <div >
+            <div className='nav-bar'>
+            <IoMdArrowDropdownCircle className='dropdown-arrow' onClick={openDropdown}/>
+            </div>
+            {open && (
+                <div className='profile-dropdown-container'>
+                <div className='dropdown-menu'>
+
+                    <ul>
+                    <div className='menu-trigger' onClick={() => alert('see your profile')}>
+                    <LuUserCircle2  className='user-icon'/>
+                    <h3 className='profile-name'>Subin B<br/><span>See your profile</span></h3>
+                    </div>
+                        <DropdownItem 
+                            Icon={MdFeedback} 
+                            text={"Give feedback"} 
+                            onClick={() => alert('Give feedback')}
+                        />
+                        <DropdownItem 
+                            Icon={RiSettings5Fill} 
+                            text={"Settings & privacy"} 
+                            EndIcon={MdNavigateNext}
+                            
+                            onClick={() => alert('Settings & privacy')}
+                
+                        />
+        
+                        <DropdownItem 
+                            Icon={IoHelpCircleSharp} 
+                            text={"Help & support"} 
+                            EndIcon={MdNavigateNext}
+                            onClick={() => alert('Help & support')}
+                        />
+                        <DropdownItem 
+                            Icon={MdDarkMode} 
+                            text={"Display & accessibility"} 
+                            EndIcon={MdNavigateNext}
+                            onClick={() => alert('Display & accessibility')}
+                        />
+                        <DropdownItem 
+                            Icon={IoLogOutSharp} 
+                            text={"Log Out"} 
+                            onClick={() => alert('Log Out clicked')}
+                        />
+                    </ul>
+                </div>
+                </div>
+            )}
+        </div>
+    );
+}
+
+function DropdownItem({ Icon, text, onClick, EndIcon  }) {
+    return (
+        <li className='dropdownitem' onClick={onClick}>
+            <Icon className='dropdown-icon'/>
+            <span>{text}</span>
+            {EndIcon && <EndIcon className='end-icon'/>}
+        </li>
+    );
+}
+
+export default Userloginitem;
